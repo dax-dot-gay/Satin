900ef344cb71f4ba638404415dd719fadb4c3598
Dax Harris <mvh3875@rit.edu>
2024-09-16 22:09:08 -0400

    new project page UI

--- satin/src/views/blank/Blank.tsx
+++ satin/src/views/blank/Blank.tsx
@@ -1,10 +1,71 @@
-import { Stack } from "@mantine/core";
-import { IconBuildingFactory2 } from "@tabler/icons-react";
+import {
+    ActionIcon,
+    Box,
+    Button,
+    Divider,
+    Group,
+    Stack,
+    Title,
+    useComputedColorScheme,
+} from "@mantine/core";
+import {
+    IconBuildingFactory2,
+    IconFolderOpen,
+    IconMapPlus,
+    IconMoonFilled,
+    IconSunFilled,
+} from "@tabler/icons-react";
+import { useTranslation } from "react-i18next";
+import { useConfig } from "../../contexts/config";
 
 export function BlankView() {
+    const { t } = useTranslation();
+    const scheme = useComputedColorScheme();
+    const [_, setColorScheme] = useConfig("colorScheme");
     return (
-        <Stack gap="sm" className="new-project-menu">
-            <IconBuildingFactory2 size={48} />
-        </Stack>
+        <Box className="new-project-root">
+            <ActionIcon
+                className="new-project-theme-switch"
+                size="36px"
+                variant={scheme === "dark" ? "light" : "filled"}
+                onClick={() =>
+                    setColorScheme(scheme === "dark" ? "light" : "dark")
+                }
+            >
+                {scheme === "dark" ? (
+                    <IconSunFilled size={20} />
+                ) : (
+                    <IconMoonFilled size={20} />
+                )}
+            </ActionIcon>
+            <Stack gap="sm" className="new-project-menu">
+                <Group gap="sm" justify="space-between">
+                    <IconBuildingFactory2 size={48} />
+                    <Stack gap={0} align="end">
+                        <Title order={2}>{t("common.appName")}</Title>
+                        <Title order={6} c="dimmed" fs="italic">
+                            {t("common.appDescription")}
+                        </Title>
+                    </Stack>
+                </Group>
+                <Divider />
+                <Button
+                    justify="space-between"
+                    px="sm"
+                    size="md"
+                    leftSection={<IconMapPlus />}
+                >
+                    {t("views.blank.createProject.button")}
+                </Button>
+                <Button
+                    justify="space-between"
+                    px="sm"
+                    size="md"
+                    leftSection={<IconFolderOpen />}
+                >
+                    {t("views.blank.openProject.button")}
+                </Button>
+            </Stack>
+        </Box>
     );
 }
