c8891b825f748191455ec7a7ffd0b1b5d2f58574
Dax Harris <mvh3875@rit.edu>
2024-09-18 12:28:20 -0400

    site selection modal

--- satin/src/modals/createFactory.tsx
+++ satin/src/modals/createFactory.tsx
@@ -1,8 +1,189 @@
-import { Stack } from "@mantine/core";
+import {
+    ActionIcon,
+    ActionIconGroup,
+    AspectRatio,
+    Button,
+    Divider,
+    Group,
+    Paper,
+    Stack,
+    Text,
+    TextInput,
+    useComputedColorScheme,
+    useMantineTheme,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
 import { ContextModalProps } from "@mantine/modals";
+import {
+    IconBuildingFactory2,
+    IconHome,
+    IconMapPinFilled,
+    IconMinus,
+    IconPlus,
+    IconX,
+} from "@tabler/icons-react";
 import { useTranslation } from "react-i18next";
+import {
+    TransformComponent,
+    TransformWrapper,
+    useControls,
+} from "react-zoom-pan-pinch";
+import { translateImageCoordinates } from "../util/satisfactoryMappings";
+import { useState } from "react";
 
-export function CreateFactoryModal({}: ContextModalProps<{}>) {
+function MapControls() {
+    const controls = useControls();
+    const theme = useComputedColorScheme();
+    return (
+        <ActionIconGroup orientation="vertical" className="map-controls">
+            <ActionIcon
+                onClick={() => controls.zoomIn()}
+                variant={theme === "dark" ? "filled" : "white"}
+                color={theme === "dark" ? "gray.9" : "gray.7"}
+            >
+                <IconPlus size={18} />
+            </ActionIcon>
+            <ActionIcon
+                variant={theme === "dark" ? "filled" : "white"}
+                color={theme === "dark" ? "gray.9" : "gray.7"}
+                onClick={() => controls.centerView(0.2)}
+            >
+                <IconHome size={18} />
+            </ActionIcon>
+            <ActionIcon
+                onClick={() => controls.zoomOut()}
+                variant={theme === "dark" ? "filled" : "white"}
+                color={theme === "dark" ? "gray.9" : "gray.7"}
+            >
+                <IconMinus size={18} />
+            </ActionIcon>
+        </ActionIconGroup>
+    );
+}
+
+export function CreateFactoryModal({ context, id }: ContextModalProps<{}>) {
     const { t } = useTranslation();
-    return <Stack gap="sm"></Stack>;
+    const form = useForm({
+        initialValues: {
+            name: "",
+            location: { x: 0, y: 0 },
+        },
+    });
+    const theme = useMantineTheme();
+    const [startPosition, setStartPosition] = useState<{
+        x: number;
+        y: number;
+    }>({ x: 0, y: 0 });
+    const mappedPosition = translateImageCoordinates(form.values.location);
+    console.log(form.values);
+    return (
+        <Stack gap="sm" className="create-factory-modal">
+            <TextInput
+                label={t("modals.createFactory.name.label")}
+                placeholder={t("modals.createFactory.name.placeholder")}
+                leftSection={<IconBuildingFactory2 size={20} />}
+                {...form.getInputProps("name")}
+            />
+            <AspectRatio ratio={1} className="location-selection-wrapper">
+                <Paper className="location-selector" shadow="md">
+                    <TransformWrapper
+                        minScale={0.2}
+                        initialScale={0.2}
+                        maxScale={1.8}
+                        centerOnInit
+                        limitToBounds={false}
+                        onTransformed={(ref) =>
+                            form.setFieldValue("location", {
+                                x:
+                                    (ref.state.positionX - startPosition.x) /
+                                    ref.state.scale,
+                                y:
+                                    (ref.state.positionY - startPosition.y) /
+                                    ref.state.scale,
+                            })
+                        }
+                        panning={{ velocityDisabled: true }}
+                        onInit={(ref) => {
+                            form.setFieldValue("location", {
+                                x: 0,
+                                y: 0,
+                            });
+                            setStartPosition({
+                                x: ref.state.positionX,
+                                y: ref.state.positionY,
+                            });
+                        }}
+                    >
+                        {() => {
+                            return (
+                                <>
+                                    <MapControls />
+                                    <IconMapPinFilled
+                                        className="selected-location"
+                                        color={theme.colors.dark[6]}
+                                    />
+                                    <TransformComponent>
+                                        <img
+                                            src="assets/map.jpg"
+                                            alt={t(
+                                                "modals.createFactory.location.alt"
+                                            )}
+                                        />
+                                    </TransformComponent>
+                                </>
+                            );
+                        }}
+                    </TransformWrapper>
+                </Paper>
+            </AspectRatio>
+            <Paper p="sm" className="paper-light">
+                <Stack gap="xs">
+                    <Group gap="sm">
+                        <IconMapPinFilled size={20} />
+                        <Text>{t("modals.createFactory.location.label")}</Text>
+                    </Group>
+                    <Group gap="sm" wrap="nowrap">
+                        <Group gap="xs" style={{ flexGrow: 1 }}>
+                            <Text>X</Text>
+                            <Paper
+                                p="xs"
+                                className="paper-dark"
+                                style={{ flexGrow: 1 }}
+                            >
+                                {mappedPosition.x}
+                            </Paper>
+                        </Group>
+                        <Divider orientation="vertical" />
+                        <Group gap="xs" style={{ flexGrow: 1 }}>
+                            <Text>Y</Text>
+                            <Paper
+                                p="xs"
+                                className="paper-dark"
+                                style={{ flexGrow: 1 }}
+                            >
+                                {mappedPosition.y}
+                            </Paper>
+                        </Group>
+                    </Group>
+                </Stack>
+            </Paper>
+            <Group gap="sm" justify="space-between" grow wrap="nowrap">
+                <Button
+                    leftSection={<IconX size={20} />}
+                    variant="light"
+                    justify="space-between"
+                    onClick={() => context.closeContextModal(id, true)}
+                >
+                    {t("actions.cancel")}
+                </Button>
+                <Button
+                    leftSection={<IconPlus size={20} />}
+                    justify="space-between"
+                    disabled={form.values.name.length === 0}
+                >
+                    {t("actions.create")}
+                </Button>
+            </Group>
+        </Stack>
+    );
 }
---
+++ satin/src/util/satisfactoryMappings.ts
@@ -0,0 +1,33 @@
+import { clamp } from "lodash";
+
+const MAP_BOUNDS = [
+    {
+        x: -3246,
+        y: -3750,
+    },
+    {
+        x: 4253,
+        y: 3750,
+    },
+];
+
+const IMAGE_SIZE = { x: 5000, y: 5000 };
+
+export function translateImageCoordinates({ x, y }: { x: number; y: number }): {
+    x: number;
+    y: number;
+} {
+    const clampedX = clamp(Math.abs(x), 0, IMAGE_SIZE.x);
+    const clampedY = clamp(Math.abs(y), 0, IMAGE_SIZE.y);
+
+    return {
+        x: Math.round(
+            (clampedX / IMAGE_SIZE.x) * (MAP_BOUNDS[1].x - MAP_BOUNDS[0].x) +
+                MAP_BOUNDS[0].x
+        ),
+        y: Math.round(
+            (clampedY / IMAGE_SIZE.y) * (MAP_BOUNDS[1].y - MAP_BOUNDS[0].y) +
+                MAP_BOUNDS[0].y
+        ),
+    };
+}
